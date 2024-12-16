33414041e5b1fd8aadd35c3c731dfea22fd9325e
xiaoxpai Name <alf_work@outlook.com>
2024-12-16 11:46:00 +0800

    doc

--- src/pages/clients/[id]/index.js
+++ src/pages/clients/[id]/index.js
@@ -1,7 +1,10 @@
+import {useRouter}   from "next/router";
 function ClientProjectsPage(){
+    const router = useRouter();
+    console.log(router.query);
     return (
         <div>
-            <h1>This is ClientProjects [id] index Page</h1>
+            <h1>This is Client  index Page</h1>
         </div>
     )
 }
